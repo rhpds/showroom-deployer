8fa6429d0dde4ac0ecfa19b5c4106c8359602437
Judd Maltin <judd@newgoliath.com>
2023-11-07 12:37:24 -0500

    add some error checks to entrypoint.sh

--- images/showroom-content/entrypoint.sh
+++ images/showroom-content/entrypoint.sh
@@ -20,6 +20,7 @@ echo "git clone the $GIT_REPO_URL into $WORKDIR"
 git clone $GIT_REPO_URL $WORKDIR || cd $WORKDIR && git pull
 cd $WORKDIR
 
+
 # Extract the name of the repository to use as the working directory
 # REPO_NAME=$(basename $GIT_REPO .git)
 
@@ -33,26 +34,32 @@ cd $WORKDIR
 # you'll have to mount the user_data.yaml into /showroom
 # OpenShift will do it with a configMap
 
+echo
 echo "Original user_data in content/antora.yml"
 cat $WORKDIR/content/antora.yml
 
 if [ -z "$USER_DATA_FILE" ]; then
+  echo
   echo "USER_DATA_FILE not defined.  Falling back to USER_DATA_FILE=/user_data/user_data.yml"
   USER_DATA_FILE: "/user_data/user_data.yml"
 fi
 
 if test -r ${USER_DATA_FILE}
 then
+  echo
   echo "Merging ${USER_DATA_FILE} and antora.yml"}
   yq -i '.asciidoc.attributes *= load("${USER_DATA_FILE}")' $WORKDIR/content/antora.yml
   echo "new user_data in content/antora.yml"
 else
+  echo
   echo "USER_DATA_FILE: ${USER_DATA_FILE} not found."
+  exit 1
 fi
 cat $WORKDIR/content/antora.yml
 echo
 
 # Run antora command (adjust the playbook file as needed)
+echo
 echo "Run antora site.yml"
 WWW_ROOT=/showroom/www/
 
@@ -60,6 +67,7 @@ test -d $WWW_ROOT || mkdir $WWW_ROOT
 
 antora --to-dir=$WWW_ROOT $ANTORA_PLAYBOOK
 
+echo
 echo "Run httpd"
 cd $WWW_ROOT
 python3 -m http.server
