f8c30ac72495f3607db69be06482138b4939da00
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-12-14 13:53:05 +0100

    Fix antora content image to use nodejs 20 UBI and observe git ref

--- images/showroom-content/entrypoint.sh
+++ images/showroom-content/entrypoint.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-if [ -z "$GIT_REPO_URL" ]; then
+if [ -z "${GIT_REPO_URL}" ]; then
   echo "GIT_REPO_URL is not set. Exiting."
   exit 1
 fi
 
-if [ -z "$ANTORA_PLAYBOOK" ]; then
+if [ -z "${ANTORA_PLAYBOOK}" ]; then
   echo "ANTORA_PLAYBOOK not defined.  Falling back to ANTORA_PLAYBOOK=default-site.yml"
   ANTORA_PLAYBOOK="default-site.yml"
 fi
 
-
 WORKDIR=/showroom/repo/
-test -d $WORKDIR && rm -rfv ${WORKDIR}
+test -d ${WORKDIR} && rm -rfv ${WORKDIR}
 
 echo
-echo "git clone the $GIT_REPO_URL into $WORKDIR"
-git clone $GIT_REPO_URL $WORKDIR
-cd $WORKDIR
+echo "git clone the ${GIT_REPO_URL} into ${WORKDIR}"
+git clone ${GIT_REPO_URL} ${WORKDIR}
+cd ${WORKDIR}
+
+if [ -z ${GIT_REPO_REF} ]; then
+  echo "No GIT_REPO_REF specified. Using default branch"
+else
+  echo "Checking out ref ${GIT_REPO_REF}"
+  git checkout ${GIT_REPO_REF}
+fi
 
 echo
 echo "Original user_data in content/antora.yml"
-cat $WORKDIR/content/antora.yml
+cat ${WORKDIR}/content/antora.yml
 
-if [ -z "$USER_DATA_FILE" ]; then
+if [ -z "${USER_DATA_FILE}" ]; then
   echo
   echo "USER_DATA_FILE not defined.  Falling back to USER_DATA_FILE=/user_data/user_data.yml"
   USER_DATA_FILE="/user_data/user_data.yml"
@@ -33,18 +39,18 @@ if test -r ${USER_DATA_FILE}
 then
   echo
   echo "Merging ${USER_DATA_FILE} and antora.yml"}
-  yq -i ".asciidoc.attributes *= load(\"${USER_DATA_FILE}\")" $WORKDIR/content/antora.yml
+  yq -i ".asciidoc.attributes *= load(\"${USER_DATA_FILE}\")" ${WORKDIR}/content/antora.yml
   echo "new user_data in content/antora.yml"
 else
   echo
   echo "USER_DATA_FILE: ${USER_DATA_FILE} not found."
   exit 1
 fi
-cat $WORKDIR/content/antora.yml
+cat ${WORKDIR}/content/antora.yml
 echo
 
 WWW_ROOT=/showroom/www/
-test -d $WWW_ROOT || mkdir $WWW_ROOT
+test -d ${WWW_ROOT} || mkdir ${WWW_ROOT}
 
 echo
 echo "Antora version"
@@ -52,15 +58,13 @@ antora --version
 
 # Run antora command (adjust the playbook file as needed)
 echo
-echo "Run antora $ANTORA_PLAYBOOK"
+echo "Run antora ${ANTORA_PLAYBOOK}"
 
-
-
-antora --to-dir=$WWW_ROOT $ANTORA_PLAYBOOK
+antora --to-dir=${WWW_ROOT} ${ANTORA_PLAYBOOK}
 
 echo
 echo "Run httpd"
-cd $WWW_ROOT
+cd ${WWW_ROOT}
 python3 -m http.server
 
 # Additional commands or logic can be added here if needed
